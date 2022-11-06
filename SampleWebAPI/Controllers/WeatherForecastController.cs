354cb0180e43b159b2d9f14ec8f09b6c0af986e6
mainaksaha <saha_mainak@hotmail.com>
2022-11-06 11:46:12 -0500

    c

--- SampleWebAPI/Controllers/WeatherForecastController.cs
+++ SampleWebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace SampleWebAPI.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -23,9 +23,49 @@ namespace SampleWebAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        [HttpGet("forecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("errortest")]
+        public IEnumerable<WeatherForecast> GetError()
+        {
+            int j = 0;
+            try
+            {
+                int k = 10 / j;
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex.Message);
+            }
+            _logger.LogInformation("Weather is not good here");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("faulttest")]
+        public IEnumerable<WeatherForecast> GetFault()
+        {
+            _logger.LogInformation("Weather is terrible here");
+            int j = 0;
+            int k = 10 / j;
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
